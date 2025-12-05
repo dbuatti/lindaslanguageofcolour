52d34d278add15690658c5b697e03f4efbc170c6
Daniele Buatti <info@danielebuatti.com>
2025-12-05 17:37:52 +1100

    [dyad] wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -9,7 +9,7 @@ const Index = () => {
       {/* Hero Section */}
       <section
         className="relative text-center bg-no-repeat bg-top bg-contain // bg-top for mobile, bg-contain for mobile
-                   min-h-[50vh] sm:h-[80vh] sm:bg-cover sm:bg-top" // Changed sm:bg-center to sm:bg-top for desktop
+                   min-h-[50vh] sm:h-[80vh] sm:bg-cover sm:bg-[50%_10%]" // Changed sm:bg-top to sm:bg-[50%_10%] for desktop
         style={{ backgroundImage: "url('/images/header-banner.jpeg')" }}
       >
         {/* The text block is now absolutely positioned at the bottom of the hero section */}
