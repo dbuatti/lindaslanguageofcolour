529fc3dd7703a01cde72fb62b1e2fe1bdb2c6498
Daniele Buatti <info@danielebuatti.com>
2025-12-05 18:20:29 +1100

    [dyad] Adding dynamic page titles for improved user experience - wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom"; // Import useLocation
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import SiteHeader from "./components/SiteHeader";
@@ -12,14 +12,50 @@ import Contact from "./pages/Contact"; // Import Contact page
 import PrivacyPolicy from "./pages/PrivacyPolicy"; // Import PrivacyPolicy page
 import TermsOfService from "./pages/TermsOfService"; // Import TermsOfService page
 import ScrollToTopButton from "./components/ScrollToTopButton"; // Import ScrollToTopButton
+import useDocumentTitle from "./hooks/useDocumentTitle"; // Import useDocumentTitle
 
 const queryClient = new QueryClient();
+
+// Component to set document title based on route
+const RouteTitleSetter = () => {
+  const location = useLocation();
+  const baseTitle = "Linda's Language of Colour";
+  let pageTitle = baseTitle;
+
+  switch (location.pathname) {
+    case "/":
+      pageTitle = baseTitle;
+      break;
+    case "/offerings":
+      pageTitle = `Offerings - ${baseTitle}`;
+      break;
+    case "/about":
+      pageTitle = `About Linda - ${baseTitle}`;
+      break;
+    case "/contact":
+      pageTitle = `Contact - ${baseTitle}`;
+      break;
+    case "/privacy":
+      pageTitle = `Privacy Policy - ${baseTitle}`;
+      break;
+    case "/terms":
+      pageTitle = `Terms of Service - ${baseTitle}`;
+      break;
+    default:
+      pageTitle = `Page Not Found - ${baseTitle}`;
+      break;
+  }
+
+  useDocumentTitle(pageTitle);
+  return null;
+};
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Sonner /> {/* Only Sonner Toaster is kept */}
       <BrowserRouter>
+        <RouteTitleSetter /> {/* Add the title setter here */}
         <div className="flex flex-col min-h-screen">
           <SiteHeader />
           <main className="flex-grow">
---
+++ src/hooks/useDocumentTitle.ts
@@ -0,0 +1,15 @@
+"use client";
+
+import { useEffect } from 'react';
+
+const useDocumentTitle = (title: string) => {
+  useEffect(() => {
+    const prevTitle = document.title;
+    document.title = title;
+    return () => {
+      document.title = prevTitle;
+    };
+  }, [title]);
+};
+
+export default useDocumentTitle;
