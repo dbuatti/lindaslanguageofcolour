84d495758b2797acdbeace70cfd21bcb7641d30e
Daniele Buatti <info@danielebuatti.com>
2025-12-05 12:56:12 +1100

    [dyad] wrote 1 file(s), deleted 1 file(s)

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,12 @@
 "use client";
 
 import React from 'react';
-import { MadeWithDyad } from './made-with-dyad';
 
 const Footer = () => {
   return (
     <footer className="bg-secondary text-secondary-foreground p-4 text-center mt-8">
       <div className="container mx-auto">
         <p className="text-sm">&copy; {new Date().getFullYear()} Linda's Language of Colour. All rights reserved.</p>
-        <MadeWithDyad />
       </div>
     </footer>
   );
